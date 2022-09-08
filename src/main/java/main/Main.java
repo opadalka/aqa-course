f7ff68cce6a7ed1ea907261697f1bb267a5920a9
oo965787 <oleksii.osypenko@telenor.se>
2022-09-08 18:19:18 +0200

    Session 3: branch demo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -26,6 +26,7 @@ public class Main {
         System.out.println(amount);
         System.out.println(isTrue);
         System.out.println("aaaaa");
+        System.out.println("cccc");
     }
 
     public static class A {
